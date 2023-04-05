9dc96d42c572b471528637401aea086f04f7890d
Enrique Noriega <enoriega@arizona.edu>
2023-04-05 15:57:53 -0700

    Fixed the equation model

--- askem_extractions/data_model/variable.py
+++ askem_extractions/data_model/variable.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from pydantic import BaseModel
 
+from askem_extractions.data_model import Equation
 from askem_extractions.data_model.data_column import DataColumn
 from askem_extractions.data_model.dkg_concept import DKGConcept
 from askem_extractions.data_model.paper import Paper
@@ -15,3 +16,4 @@ class Variable(BaseModel):
     dkg_groundings: list[DKGConcept]
     column: list[DataColumn] = []       # Default value of empty list
     paper: Paper
+    equations: list[Equation] = []      # Optional equations to which this variable is associated
