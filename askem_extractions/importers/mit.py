1de94a299c3d15a4f5ac46b8e559658e8abc31e8
chunwei <chunwei@uchicago.edu>
2023-04-07 22:24:13 -0400

    refactor mit code module

--- askem_extractions/importers/mit.py
+++ askem_extractions/importers/mit.py
@@ -1,10 +1,11 @@
 import json
 from pathlib import Path
 
-from askem_extractions.data_model import ExtractionsCollection, DKGConcept, Dataset, DataColumn
+from askem_extractions.data_model import ExtractionsCollection, DKGConcept, Dataset, DataColumn, Variable, \
+    VariableStatement, VariableMetadata
 
 
-def import_mit(a_path: Path, m_path: Path, map_path: Path) -> ExtractionsCollection:
+def import_mit_and_merge(a_path: Path, m_path: Path, map_path: Path) -> ExtractionsCollection:
     # Extract the data from json file
     from_json = ExtractionsCollection.from_json(a_path)
 
@@ -60,5 +61,106 @@ def import_mit(a_path: Path, m_path: Path, map_path: Path) -> ExtractionsCollect
     from_json.save_json("TA1-integration.json")
 
 
-if __name__ == "__main__":
-    import_mit(Path("../../examples/temp.json"), Path("../../examples/mit_extraction.json"), Path("../../examples/mapping.txt"))
+
+def import_mit(m_path: Path) -> ExtractionsCollection:
+    collection = []
+
+    with open(m_path, "r") as file_a:
+        data_m = json.load(file_a)
+
+    for entry_a in data_m:
+        id = entry_a["id"]
+        name = entry_a["name"]
+        text_annotations = entry_a["text_annotations"]
+        dkg_groundings = []
+        metadata = []
+        # if entry_a["dkg_annotations"] is not empty
+        if entry_a["dkg_annotations"]:
+            # iterate through the list of dkg_annotations
+            for term in entry_a["dkg_annotations"]:
+                if len(term) < 2:
+                    continue
+                dkg = DKGConcept(
+                    name=term[1],
+                    id=term[0],
+                )
+                dkg_groundings.append(dkg)
+        columns = []
+        # if entry_a["data_annotations"] is empty
+        if entry_a["data_annotations"]:
+            # iterate through the list of data_annotations
+            for term in entry_a["data_annotations"]:
+                dataset = Dataset(
+                    name=term[1],
+                    id=term[0],
+                )
+                col = DataColumn(
+                    name=term[3],
+                    id=str(term[0]) + "-" + str(term[2]),
+                    dataset=dataset,
+                )
+                columns.append(col)
+        # if text_annotations is not empty
+        if text_annotations:
+            for term in text_annotations:
+                md = VariableMetadata(
+                    type="text_annotation",
+                    value=term,
+                )
+                metadata.append(md)
+
+        variable = Variable(
+            id=id,
+            name=name,
+            metadata=metadata,
+            dkg_groundings=dkg_groundings,
+            column=columns,
+        )
+        variable_statement = VariableStatement(
+            id=id,
+            variable=variable,
+        )
+        collection.append(variable_statement)
+
+    return ExtractionsCollection(variable_statements=collection)
+
+
+def merge_collections(a_collection: ExtractionsCollection, m_collection: ExtractionsCollection, map_path: Path) -> ExtractionsCollection:
+    # Extract the data from json file
+    # Load mapping file
+    with open(map_path, "r") as mapping_file:
+        mappings = mapping_file.readlines()
+
+    # Parse the mappings into a dictionary
+    mapping_dict = {}
+    for mapping in mappings:
+        key, value = mapping.strip().split(": ")
+        mapping_dict[key] = value.strip('"').strip(",")
+
+    for vs in a_collection.variable_statements:
+        entry_b_id = vs.id
+        print(entry_b_id)
+        if entry_b_id in mapping_dict.values():
+            print("Found mapping")
+            # Get the corresponding key (id from data_a) and find the entry in data_a
+            entry_a_id = [k for k, v in mapping_dict.items() if v == entry_b_id][0]
+            for entry_a in m_collection.variable_statements:
+                if entry_a.id == entry_a_id:
+                    if entry_a.variable.metadata:
+                        for md in entry_a.variable.metadata:
+                            md.type = entry_a.variable.name
+                            vs.variable.metadata.append(md)
+                    # if entry_a.variable.dkg_groundings is not empty
+                    if entry_a.variable.dkg_groundings:
+                        # iterate through the list of dkg_annotations
+                        for term in entry_a.variable.dkg_groundings:
+                            vs.variable.dkg_groundings.append(term)
+                    # if entry_a.variable.column is not empty
+                    if entry_a.variable.column:
+                        # iterate through the list of data_annotations
+                        for term in entry_a.variable.column:
+                            vs.variable.column.append(term)
+                    # if entry_a["equation_annotations"] is empty
+    return ExtractionsCollection(variable_statements=a_collection.variable_statements)
+
+
--- examples/usage.py
+++ examples/usage.py
@@ -7,25 +7,43 @@ from pathlib import Path
 
 from askem_extractions.data_model import *
 from askem_extractions.importers import import_arizona, import_mit
+from askem_extractions.importers.mit import merge_collections
 
 if __name__ == "__main__":
     # Import Arizona extractions into our data model
     path_to_json = Path(__file__).parent / Path("arizona_output_example.json")
-    collection = import_arizona(path_to_json)
+    a_collection = import_arizona(path_to_json)
 
     # Save the collection of arizona extractions as the standard json format
-    collection.save_json("temp.json")
+    a_collection.save_json("a_temp.json")
 
     # Reloads the collection from the json file
-    deserialized = ExtractionsCollection.from_json("temp.json")
+    deserialized = ExtractionsCollection.from_json("a_temp.json")
 
     # Both collections should be equal. Since we are using pydantic, it will do a deep comparison
-    assert collection == deserialized, "Deserialization didn't work"
+    assert a_collection == deserialized, "Deserialization didn't work"
 
     # Print the collection
     print("Arizona Worked!!!")
 
-    # Import MIT extractions into our data model with the mapping file
-    import_mit(Path("temp.json"), Path("mit_extraction.json"),
-               Path("mapping.txt"))
+    # Import MIT extractions into our data model
+    path_to_json = Path(__file__).parent / Path("mit_extraction.json")
+    m_collection = import_mit(path_to_json)
+
+    # Save the collection of arizona extractions as the standard json format
+    m_collection.save_json("m_temp.json")
+
+    # Reloads the collection from the json file
+    deserialized = ExtractionsCollection.from_json("m_temp.json")
+
+    # Both collections should be equal. Since we are using pydantic, it will do a deep comparison
+    assert m_collection == deserialized, "Deserialization didn't work"
+
+    # Print the collection
+    print("MIT Worked!!!")
+    #
+    # Merge both data model with the mapping file
+    merged = merge_collections(a_collection, m_collection,
+                               Path("mapping.txt"))
+    merged.save_json("ta1-bucky_extraction_v1.json")
 
