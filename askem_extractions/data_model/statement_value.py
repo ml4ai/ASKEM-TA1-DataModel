869af56d1c52555cbb6c443bde2bc13d59848a26
Enrique Noriega <enoriega@arizona.edu>
2023-04-27 18:25:55 -0700

    Small bug fix

--- askem_extractions/data_model/statement_value.py
+++ askem_extractions/data_model/statement_value.py
@@ -9,7 +9,7 @@ from askem_extractions.data_model.statement_value_type import StatementValueType
 class StatementValue(BaseModel):
     """ Represents the contents of a statement about a variable """
     value: str                                   # Can be either numeric or a string or a tuple with both
-    type: StatementValueType                     # The type of the statement (see the enum for different types)
+    type: str                     # The type of the statement (see the enum for different types)
     dkg_grounding: Optional[DKGConcept]          # Optional grounding of the statement to a DKG Concept
 
     class Config:
